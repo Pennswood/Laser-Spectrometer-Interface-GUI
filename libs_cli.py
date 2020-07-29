d6417ff38fd6a9c251036c7479b08446b9dd5144
ExpandingDev <tylersengia@gmail.com>
2020-07-28 23:43:50 -0400

    Added arguments parser

--- libs_cli.py
+++ libs_cli.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-import seabreeze
-import laser_control
-import random
+import struct
+from argparse import ArgumentParser
 
+import seabreeze
+from seabreeze.spectrometers import Spectrometer
+import laser_control
+
+# Created using Notepad++. I have no regrets. And notice that it works. :)
+
+def main():
+    parser = ArgumentParser(description="CLI for performing LIBS using an Ocean Optics FLAME-T spectrometer and a 1064nm Quantum Composers MicroJewel laser.",
+    epilog="Created for the 2020 NASA BIG Idea challenge, Penn State Oasis team. Questions: tylersengia@gmail.com")
+
+    parser.add_argument("--spec-dev", "-s", "spec-dev", help="Specify the USB device for the spectrometer. Default is autodetected by seabreeze.", default=None, )
+
+
+if __name__ == "__main__":
+    main()
