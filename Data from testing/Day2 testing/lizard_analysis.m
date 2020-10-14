38796b0c61449899f6ed2c662779cb9c4e3ae015
pennswood <normenyu@gmail.com>
2020-10-13 23:43:32 -0400

    deleted unwanted files

--- Data from testing/Day2 testing/lizard_analysis.m
+++
@@ -1,6 +0,0 @@
-no_optics_intensities = csvread('lno/10000us_avg.csv');
-optics_intensities = csvread('lo/10000us_avg.csv');
-wavelength = readtable('wavelength_chart.csv').Var1;
-ratio = (optics_intensities-3000)./(no_optics_intensities-3000);
-plot(wavelength(70:300), ratio(70:300));
-hold on;
